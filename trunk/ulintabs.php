532abbb9d4da758427704bd99c52c3f2ebdba5ca
timorning <timorning@2ef79e72-db17-11de-aa1f-bbd297b2055c>
2009-12-02 15:04:24 +0000

    nodemarquee

---
+++ trunk/ulintabs.php
@@ -0,0 +1,10 @@
+<?php
+echo "<ul>
+      <li><a href='#s01'><span>首页</span></a></li>
+      <li><a href='#s02'><span>点击赚钱</span></a></li>
+      <li><a href='#s03'><span>发布广告</span></a></li>
+      <li><a href='#s04'><span>留言板</span></a></li>
+      <li><a href='#s05'><span>我的账户</span></a></li>
+    </ul>
+"
+?>
