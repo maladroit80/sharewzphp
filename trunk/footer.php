78cc6414bfd6f45ad95dba52f22284abfc45a206
hilary3210 <hilary3210@2ef79e72-db17-11de-aa1f-bbd297b2055c>
2009-12-11 01:34:30 +0000

    Change footer.php

--- trunk/footer.php
+++ trunk/footer.php
@@ -1,9 +1,54 @@
-<div id="footer">
-    <hr class="noscreen" />
-    <p class="f-right noprint"><a href="category.html">Categorys</a>, <a href="detail.html">Details</a>, <a href="results.html">Results</a>, <a href="submit.html">Submit</a></p>
-    <p>&copy;&nbsp;2009 <a href="#">Your Company</a><br />
-      <!-- Do you want to remove this backlinks? Look at www.nuviotemplates.com/payment.php -->
-      <span id="copy"><a href="http://www.nuviotemplates.com/">Free web templates</a> by <a href="http://www.qartin.cz/">Qartin</a><br />
-      <span class="smaller">Visit <a href="http://www.88web.org/" title="免费模板下载">免费模板下载</a></span></span></p>
-    <!-- Do you want to remove this backlinks? Look at www.nuviotemplates.com/payment.php -->
+  <div id="footer">
+<p>&copy; 2008 <a href="index.php"><?php include('sitename.php');?>
+有限公司</a> 版权所有<a href="tos.php"></a> | <a href="privacy.php">隐私</a> | <a href="tos.php">条款</a> | <a href="faq.php">帮助</a> | <a href="payment_proof.php">付款证明</a>  | <a href="contact.php">联系我们</a> 客服邮箱<span style="font-family: Geneva, Arial, Helvetica, sans-serif">:<span style="font-family: Arial, Helvetica, sans-serif; color: #FF0000">masteradsnet@gmail.com</span></span>
+<table align="center" width="100%" cellpadding="0">
+  <tr bgcolor="#006600">
+    <td height="5" colspan="8"><div align="left"></div></td>
+  </tr>
+  <tr>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="http://www.googlebux.cn/honest/bank/paypal.html">PP网银教程</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="http://googlebux.cn/buxscript.php">Googlebux程序</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="http://www.googlebux.cn/honest/bank/alertpay.html">AP网银教程</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="buxscript.php">Masterads程序</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="http://www.googlebux.cn/honest/script.html" target="_blank">中英bux程序</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"> <a href="buxscript.php">Masterads程序</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="http://www.googlebux.cn/honest/script.html" target="_blank">中英bux程序</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+  </tr>
+  <tr>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+  </tr>
+  <tr>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+    <td><div align="center" style="font-size: 12px; color: #000000"><a href="contact.php">友情链接</a></div></td>
+  </tr>
+  <tr bgcolor="#006600">
+    <td height="4" colspan="8"><div align="center" style="font-size: 12px; color: #000000"></div>      </td>
+    </tr>
+</table>
+<p><br>
+  <a href="http://validator.w3.org/check?uri=referer"></a>
+  </div>
 </div>
+<script type="text/javascript">
+var tooltipObj = new DHTMLgoodies_formTooltip();
+tooltipObj.setTooltipPosition('right');
+tooltipObj.setPageBgColor('#EEEEEE');
+tooltipObj.setTooltipCornerSize(10);
+tooltipObj.initFormFieldTooltip();
+</script>
+</body>
+</html>
