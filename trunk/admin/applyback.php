87da17a6b687f6ff11045bae397e4d7b51c58fb0
hilary3210 <hilary3210@2ef79e72-db17-11de-aa1f-bbd297b2055c>
2010-03-26 02:24:43 +0000

    返佣后台系统初步开发完成（hilary 03/26 update）

--- trunk/admin/applyback.php
+++ trunk/admin/applyback.php
@@ -4,6 +4,7 @@
 if (isset($_POST["id"]))
 {
 $id=$_POST["id"];
+$siteid = $_POST["siteid"];
 $username=$_POST["username"];
 $backname=$_POST["backname"];
 $sitename = $_POST["sitename"];
@@ -15,6 +16,18 @@ $siteregtime = $_POST["siteregtime"];
     mysql_query($query) or die(mysql_error());
 
     echo "<font color=\"green\"><b>成功登记.</b></font><br><br>";
+    if($siteregstatus=='成功登记'){
+    	
+    $sqlrefernumber = mysql_query("select * from tb_back_site where site_id ='$siteid'");
+    $row = mysql_fetch_array($sqlrefernumber);
+    $refernumber = $row["now_refer_number"]+1;
+    
+    $sql="UPDATE tb_back_site SET now_refer_number='$refernumber' where site_id='$siteid'";
+    $query = "UPDATE tb_back_site SET now_refer_number='$refernumber' where site_id='$siteid'";
+    mysql_query($query) or die(mysql_error());
+
+    echo "<font color=\"green\"><b>此站点登记人数已经增加一个.</b></font><br><br>";
+}
 }
 
 if (isset($_GET["id"]))
@@ -35,7 +48,9 @@ while ($registroe = mysql_fetch_array($tablae)) { // comienza un bucle que leera
 
 编号: <input type="hidden" name="id" value="<?php echo $registroe["id"] ?>"><?php echo $registroe["id"] ?><br>
 站点名: 
-<input type="text" name="sitename" value="<?php echo $registroe["site_name"] ?>">
+<input type="text" name="sitename" value="<?php echo $registroe["site_name"] ?>"><br>
+站点id: 
+<input type="text" name="siteid" value="<?php echo $registroe["site_id"] ?>">
 <br>
 本站用户名: 
 <input type="text" name="username" value="<?php echo $registroe["username"] ?>">
@@ -68,6 +83,7 @@ if ($option=="delete"){
 <table cellspacing="0" cellpadding="0">
 <tr>
 <th>编号</th>
+<th>站点id</th>
 <th>站点名</th>
 <th>本站用户名</th>
 <th>登记站点用户名</th>
@@ -86,19 +102,20 @@ while ($registro = mysql_fetch_array($tabla)) { // comienza un bucle que leera t
 echo "
 <tr>
 <td>". $registro["id"] ."</td>
+<td>". $registro["site_id"] ."</td>
+<td>". $registro["site_name"] ."</td>
 <td>". $registro["username"] ."</td>
 <td>". $registro["backname"] ."</td>
-<td>". $registro["site_name"] ."</td>
 <td>". $registro["site_reg_status"] ."</td>
 <td>". $registro["site_reg_time"] ."</td>
 <td>";
 ?>
-<form method="post" action="index.php?op=58&id=<?php echo $registro["id"] ?>&option=edit">
+<form method="post" action="index.php?op=58&id=<?php echo $registro["id"] ?>&siteid=<?php echo $registro["site_id"] ?>&option=edit">
 <input type="submit" value="编辑" class="button">
 </form>
 </td>
 <td>
-<form method="post" action="index.php?op=58&id=<?php echo $registro["id"] ?>&option=delete">
+<form method="post" action="index.php?op=58&id=<?php echo $registro["id"] ?>&siteid=<?php echo $registro["site_id"] ?>&option=delete">
 <input type="submit" value="删除" class="button">
 </form>
 </td>
--- trunk/admin/viewbacksite.php
+++ trunk/admin/viewbacksite.php
@@ -7,6 +7,7 @@ if(isset($_POST["id"]))
 	
 $id=$_POST["id"];
 $username=$_POST["username"];
+$siteid=$_POST["siteid"];
 $backname=$_POST["backname"];
 $sitename = $_POST["sitename"];
 $siteregstatus = $_POST["siteregstatus"];
@@ -17,6 +18,18 @@ $siteregtime = $_POST["siteregtime"];
     mysql_query($query) or die(mysql_error());
 
     echo "<font color=\"green\"><b>编辑成功.</b></font><br><br>";
+    if($siteregstatus=='成功登记'){
+    	
+    $sqlrefernumber = mysql_query("select * from tb_back_site where site_id ='$siteid'");
+    $row = mysql_fetch_array($sqlrefernumber);
+    $refernumber = $row["now_refer_number"]+1;
+    
+    $sql="UPDATE tb_back_site SET now_refer_number='$refernumber' where site_id='$siteid'";
+    $query = "UPDATE tb_back_site SET now_refer_number='$refernumber' where site_id='$siteid'";
+    mysql_query($query) or die(mysql_error());
+
+    echo "<font color=\"green\"><b>此站点登记人数已经增加一个.</b></font><br><br>";
+}
 }
 
 if (isset($_GET["id"]))
@@ -36,6 +49,9 @@ while ($registroe = mysql_fetch_array($tablae)) { // comienza un bucle que leera
 <form method="post" action="index.php?op=611">
 
 编号: <input type="hidden" name="id" value="<?php echo $registroe["id"] ?>"><?php echo $registroe["id"] ?><br>
+站点id: 
+<input type="text" name="siteid" value="<?php echo $registroe["site_id"] ?>">
+<br>
 站点名: 
 <input type="text" name="sitename" value="<?php echo $registroe["site_name"] ?>">
 <br>
@@ -103,12 +119,12 @@ echo "
 <td>". $registro["site_reg_time"] ."</td>
 <td>";
 ?>
-<form method="post" action="index.php?op=611&id=<?php echo $registro["id"] ?>&option=edit">
+<form method="post" action="index.php?op=611&id=<?php echo $registro["id"] ?>&siteid=<?php echo $registro["site_id"] ?>&option=edit">
 <input type="submit" value="编辑/登记返佣" class="button">
 </form>
 </td>
 <td>
-<form method="post" action="index.php?op=611&id=<?php echo $registro["id"] ?>&option=delete">
+<form method="post" action="index.php?op=611&id=<?php echo $registro["id"] ?>&siteid=<?php echo $registro["site_id"] ?>&option=delete">
 <input type="submit" value="删除" class="button">
 </form>
 </td>
