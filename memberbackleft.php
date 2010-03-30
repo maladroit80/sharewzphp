956af6d95188facec6b20f93438fc7bb8f4bc48a
hilary3210 <hilary3210@2ef79e72-db17-11de-aa1f-bbd297b2055c>
2010-03-30 09:36:29 +0000

    0330 update by hilary

--- memberbackleft.php
+++ memberbackleft.php
@@ -5,13 +5,13 @@ require("config.php");
 	<div class="channel">	
 	<ul>
 	<li><a href="back.php" >返佣首页</a></li>
-	<li><a href="click.php" >国内点击赚钱网站</a></li>
-	<li><a href="foreignclick.php" >国外点击赚钱网站</a></li>
-	<li><a href="task.php">注册赚钱网站</a></li>
-	<li><a href="survey.php">调查赚钱网站</a></li>
-	<li><a href="vote.php" >投票赚钱网站</a></li>
-	<li><a href="surf.php" >冲浪赚钱网站</a></li>
-	<li><a href="linglei.php" >另类赚钱网站</a></li>
+	<li><a href="sites_click.php" >国内点击赚钱网站</a></li>
+	<li><a href="sites_foreignclick.php" >国外点击赚钱网站</a></li>
+	<li><a href="sites_task.php">注册赚钱网站</a></li>
+	<li><a href="sites_survey.php">调查赚钱网站</a></li>
+	<li><a href="sites_vote.php" >投票赚钱网站</a></li>
+	<li><a href="sites_surf.php" >冲浪赚钱网站</a></li>
+	<li><a href="sites_linglei.php" >另类赚钱网站</a></li>
 	</ul>
 	
   <hr width="150!"! size="2">
